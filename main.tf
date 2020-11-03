fd7f70a9720b1e6658a187020743af1c73ea1c7c
Anton <AMiller@yandex.ru>
2020-11-03 14:14:54 +1100

    buildimg image v0.4

--- main.tf
+++ main.tf
@@ -31,7 +31,7 @@ provider "aws" {
 
 resource "aws_key_pair" "ssh_key" {
     key_name   = "ssh_key"
-    public_key = "${file(var.public_key_path)}" 
+    public_key = file(var.public_key_path) 
 }
 
 #resource "aws_network_interface" "dev_if" {
